c0a728277a4971dbeea44166be03a43bdf8825a6
trantien1701 <trannhattien170102@gmail.com>
2023-06-05 16:45:29 +0700

    .

---
+++ src/app/Hearder/Hearder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Hearder',
+  templateUrl: './Hearder.component.html',
+  styleUrls: ['./Hearder.component.css']
+})
+export class HearderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
